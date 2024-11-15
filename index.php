b8cbd02edb9793dd592cffc4acfcc1f71c89ec4e
mnishom <m.nishom.dosen@gmail.com>
2024-11-15 17:08:37 +0700

    Initial commit

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Aplikasi Kepegawaian</title>
+    <link rel="icon" type="image/x-icon" href="images/logo2.ico">
 
     <!-- Link ke CSS Bootstrap -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
