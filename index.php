35a6ece34aba77f9e2df23c2fc19bba9c94b0020
mnishom <m.nishom.dosen@gmail.com>
2024-11-15 17:05:34 +0700

    Initial commit

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Halaman Web dengan Bootstrap</title>
+    <title>Aplikasi Kepegawaian</title>
+
     <!-- Link ke CSS Bootstrap -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="styles/style.css">
